1858d28cafe34f6d5802fdafa70beff3b58b8764
Irvingzjh <Irvingzjh@gmail.com>
2024-06-26 10:29:48 +0800

    modified:   book/42is.py

--- book/42is.py
+++ book/42is.py
@@ -14,3 +14,41 @@ def double_and_print(x):
     return x * 2
 s = range(3, 9)
 doub = map(double_and_print, s)
+
+
+# With our knowledge of iterators, we can implement the execution rule of a for statement
+# In terms of while, assignment, and try statements.
+counts = [1, 2, 3]
+items = counts.__iter__()
+try:
+    while True:
+        item = items.__next__()
+        print(item)
+except StopIteration:
+        pass
+# Alter but more common way
+for item in counts:
+     print(item)
+"""
+1
+2
+3
+1
+2
+3
+"""    
+
+
+a = [1, 2, 3, 2]
+b = [4, 5, 6]
+for pair in zip(a, b):
+    print(pair)
+    print(type(pair))
+"""
+(1, 4)
+<class 'tuple'>
+(2, 5)
+<class 'tuple'>
+(3, 6)
+<class 'tuple'>
+"""
