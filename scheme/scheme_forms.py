06d94a9f0b47bc92e6f49c7cc3fd754e8cd7575f
Irvingzjh <Irvingzjh@gmail.com>
2024-07-19 10:16:06 +0800

    modified:   scheme/scheme_forms.py

--- scheme/scheme_forms.py
+++ scheme/scheme_forms.py
@@ -37,6 +37,10 @@ def do_define_form(expressions, env):
         validate_form(expressions, 2, 2) # Checks that expressions is a list of length exactly 2
         # BEGIN PROBLEM 4
         "*** YOUR CODE HERE ***"
+        # The operator should be evaluated only a single time before raising an error (because x is not bound to a procedure).
+        # So, expressions.rest.first not expressions.rest
+        env.define(signature, scheme_eval(expressions.rest.first, env))
+        return signature
         # END PROBLEM 4
     elif isinstance(signature, Pair) and scheme_symbolp(signature.first):
         # defining a named procedure e.g. (define (f x y) (+ x y))
@@ -57,6 +61,7 @@ def do_quote_form(expressions, env):
     validate_form(expressions, 1, 1)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    
     # END PROBLEM 5
 
 def do_begin_form(expressions, env):
